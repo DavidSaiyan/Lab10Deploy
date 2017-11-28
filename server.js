374ed96ab282c62dd61f28e4e038951ed17b2926
DavidSaiyan <dsh9b@mail.umkc.edu>
2017-11-27 18:06:27 -0600

    updated port

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+http.listen(port, function(){
+    console.log('listening on *:' + port);
 });
 
 io.on('disconnect', function(){
@@ -46,4 +46,3 @@ app.use('/api', router);
 
 //Start Server
 //app.listen(port);
-console.log('Application Port is:  ' + port);

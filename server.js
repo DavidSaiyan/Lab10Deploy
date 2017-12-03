82b9db24f2fa357b081a69071b3d6be51553bbf4
DavidSaiyan <dsh9b@mail.umkc.edu>
2017-12-02 21:19:12 -0600

    changed cors settings

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(bodyParser({limit: '50mb'}));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });

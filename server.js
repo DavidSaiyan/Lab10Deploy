8f667f712a2446cbf00109ea89318b8491301be9
DavidSaiyan <dsh9b@mail.umkc.edu>
2017-12-02 21:00:53 -0600

    added CORS to project

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app        = express();
 var bodyParser = require('body-parser');
 var http = require('http').Server(app);
 var io     = require("socket.io")(http);
+var cors = require('cors');
 
 var db = require('./app/services/database.js');
 db.connectSql();
@@ -17,6 +18,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+app.use(cors());
 
 var port = process.env.PORT || 4215;
 var router = express.Router();

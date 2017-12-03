9fbffc0d6db41516069c239770030cd4255400a3
DavidSaiyan <dsh9b@mail.umkc.edu>
2017-12-02 20:20:11 -0600

    added more endpoints for chat

--- app/services/database.js
+++ app/services/database.js
@@ -1,5 +1,4 @@
 const sql = require('mssql');
-
 const config = {
     user: 'team3_app_user',
     password: 'zrdUceIeljaKSkP45px6',
@@ -10,19 +9,28 @@ const config = {
     }
 };
 
+var pool = '';
+
+function connectSql(){
+    sql.connect(config).then(function(p){
+        pool = p;
+    })
+}
+
+sql.on('error', function(err){
+    console.log(err);
+});
+
+
 function SignIn(user){
-    close();
-    return sql.connect(config).then(function(pool){
-        return pool.request()
-            .input('Username', sql.NVarChar, user.username)
-            .input('Password', sql.NVarChar, user.password)
-            .execute('SignIn')
-    });
+    return pool.request()
+        .input('Username', sql.NVarChar, user.username)
+        .input('Password', sql.NVarChar, user.password)
+        .execute('SignIn')
 }
 
 function RegisterUser(user){
     console.log(user);
-    return sql.connect(config).then(function(pool){
         return pool.request()
             .input('Username', sql.NVarChar, user.username)
             .input('Password', sql.NVarChar, user.password)
@@ -36,41 +44,53 @@ function RegisterUser(user){
             .input('Provider', sql.NVarChar, user.provider)
             .input('Birthday', sql.NVarChar, user.birthday)
             .execute('RegisterUser')
-    });
 }
 
 function getRooms(user){
-    close();
-    return sql.connect(config).then(function(pool){
         return pool.request()
             .input('UserId', sql.NVarChar, user.userId)
             .input('Password', sql.NVarChar, user.password)
             .execute('GetRooms')
-    });
 }
 
 function getGuests(room){
-    close();
-    return sql.connect(config).then(function(pool){
         return pool.request()
             .input('RoomId', sql.NVarChar, room.roomId)
             .execute('GetGuests')
-    });
+}
+
+function search(query, userId, roomId){
+    return pool.request()
+        .input('query', sql.NVarChar, query)
+        .input('userId', sql.NVarChar, userId)
+        .input('roomId', sql.NVarChar, roomId)
+        .execute('Search')
+}
+
+function InsertMessage(msg){
+    console.log(msg)
+    return pool.request()
+        .input('RoomId', sql.NVarChar, msg.ChatId)
+        .input('UserId', sql.NVarChar, msg.UserId)
+        .input('MessageText', sql.NVarChar, msg.Message)
+        .input('LanguageCode', sql.NVarChar, msg.Language)
+        .execute('InsertMessage')
+}
+
+function GetMessages(roomId){
+    return pool.request()
+        .input('RoomId', sql.NVarChar, roomId)
+        .execute('GetMessages')
 }
 
 
 function checkIfUserExists(username){
-    close();
-    return sql.connect(config).then(function(pool){
         return pool.request()
             .input('username', sql.NVarChar, username)
             .query('select username from Users where username = @username')
-    });
 }
 
 function addRoom(data){
-    close();
-    return sql.connect(config).then(function(pool){
         return pool.request()
             .input('name', sql.NVarChar, data.name)
             .input('locationId', sql.Int, 1)
@@ -80,56 +100,41 @@ function addRoom(data){
             .input('PrimaryCode', sql.NVarChar, data.primCode)
             .input('SecondaryCode', sql.NVarChar, data.secCode)
             .execute('AddRoom')
-    });
 }
 
 
 function addGuest(data){
-    close();
-    return sql.connect(config).then(function(pool){
         return pool.request()
             .input('RoomId', sql.NVarChar, data.roomId)
             .input('GuestId', sql.Int, data.userId)
             .execute('AddGuest')
-    });
 }
 
 function removeGuest(data){
-    close();
-    return sql.connect(config).then(function(pool){
         return pool.request()
             .input('RoomId', sql.NVarChar, data.roomId)
             .input('GuestId', sql.Int, data.userId)
             .execute('RemoveGuest')
-    });
 }
 
 function getLanguages(){
-    close();
-    return sql.connect(config).then(function(pool){
         return pool.request()
             .query('select LanguageCode, LanguageDescription from Languages')
-    });
 }
 
 
 function addFriend(data) {
-    close();
-    return sql.connect(config).then(function(pool){
         return pool.request()
             .input('UserId', sql.Int, data.userId)
             .input('FriendId', sql.Int, data.friendId)
             .execute('addFriend')
-    });
 }
 
 function getFriends(data){
-    close();
-    return sql.connect(config).then(function(pool){
+
         return pool.request()
             .input('UserId', sql.Int, data.userId)
             .execute('GetUserFriends')
-    })
 }
 
 function close(){
@@ -137,6 +142,7 @@ function close(){
 }
 
 module.exports = {
+    connectSql: connectSql,
     SignIn: SignIn,
     RegisterUser: RegisterUser,
     checkIfUserExists: checkIfUserExists,
@@ -148,5 +154,8 @@ module.exports = {
     addGuest: addGuest,
     removeGuest: removeGuest,
     getGuests: getGuests,
+    search: search,
+    InsertMessage: InsertMessage,
+    GetMessages: GetMessages,
     close: close
 };
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var http = require('http').Server(app);
 var io     = require("socket.io")(http);
 
 var db = require('./app/services/database.js');
+db.connectSql();
 
 //Configure App
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -26,7 +27,6 @@ app.get('/', function(req, res){
 
 app.get('/languages', function (req, res){
     db.getLanguages().then(function (data){
-        db.close();
         res.json(data.recordset);
     },
     function (err){
@@ -38,7 +38,6 @@ app.get('/languages', function (req, res){
 
 app.post('/signin', function (req, res){
     db.SignIn(req.body).then(function (data){
-            db.close();
             res.json(data.recordset);
         },
         function (err){
@@ -49,9 +48,7 @@ app.post('/signin', function (req, res){
 });
 
 app.post('/getrooms', function (req, res){
-    db.close();
     db.getRooms(req.body).then(function (data){
-            db.close();
             res.json(data.recordset);
         },
         function (err){
@@ -64,12 +61,10 @@ app.post('/getrooms', function (req, res){
 app.post('/addguest', function (req, res){
     console.log('adding guest');
     db.addGuest(req.body).then(function (data){
-            db.close();
             res.json('successfully added guest.');
         },
         function (err){
             console.log(err);
-            db.close();
             return err;
         })
 });
@@ -77,12 +72,10 @@ app.post('/addguest', function (req, res){
 app.post('/removeguest', function (req, res){
     console.log('adding guest');
     db.removeGuest(req.body).then(function (data){
-            db.close();
             res.json('Successfully removed guest');
         },
         function (err){
             console.log(err);
-            db.close();
             return err;
         })
 });
@@ -90,7 +83,6 @@ app.post('/removeguest', function (req, res){
 
 app.post('/getguests', function (req, res){
     db.getGuests(req.body).then(function (data){
-            db.close();
             res.json(data.recordset);
         },
         function (err){
@@ -102,70 +94,85 @@ app.post('/getguests', function (req, res){
 
 app.post('/addroom', function (req, res){
     db.addRoom(req.body).then(function (data){
-            db.close();
             res.json(data.recordset);
         },
         function (err){
             console.log(err);
-            db.close();
             return err;
         })
 });
 
 app.post('/addfriend', function (req, res){
     db.addFriend(req.body).then(function (data){
-            db.close();
             res.json('You are now friends with ' + req.body.name);
         },
         function (err){
             console.log(err);
-            db.close();
+            return err;
+        })
+});
+
+app.get('/search/:query/:userId/:roomId', function (req, res){
+    db.search(req.params.query).then(function (data){
+        console.log(data);
+            res.json(data.recordset);
+        },
+        function (err){
+            console.log(err);
             return err;
         })
 });
 
 app.post('/getfriends', function (req, res){
     db.getFriends(req.body).then(function (data){
-            db.close();
             res.json(data.recordset);
         },
         function (err){
             console.log(err);
-            db.close();
             return err;
         })
 });
+
+app.get('/getmessages/:roomId', function (req, res){
+    db.GetMessages(req.params.roomId).then(function (data){
+            res.json(data.recordset);
+        },
+        function (err){
+            console.log(err);
+            return err;
+        })
+});
+
 
 app.post('/register', function (req, res){
     db.checkIfUserExists(req.body.username).then(function (resp){
         //console.log(resp.recordset[0].username);
         if(resp.recordset.length > 0 && resp.recordset[0].username === req.body.username){
-            db.close();
             res.json('The user already exists');
         }
         db.close();
         db.RegisterUser(req.body).then(function (data){
-                db.close();
                 res.json('Successfully registered ' + req.body.username);
             },
             function (err){
                 console.log(err);
-                db.close();
                 return err;
             })
     }, function (err){
         console.log(err);
-        db.close()
         return err;
     })
 });
 
+io.removeAllListeners();
 
 io.on('connection', function(socket){
+    socket.removeAllListeners();
     console.log('a user connected');
     socket.on('msg', function(data){
         console.log('message: ' + data.Message);
         console.log(data.ChatId);
+        db.InsertMessage(data);
         io.emit(data.ChatId, data);
     });
 
